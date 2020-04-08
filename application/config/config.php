d2db4e1ec04e2efa9b5af9882b0136dc29260dcc
parth <parth.p.ajudiya@gmail.com>
2020-04-09 00:42:52 +0530

    config update

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/q_erp/';
+$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/query/';
 
 /*
 |--------------------------------------------------------------------------
