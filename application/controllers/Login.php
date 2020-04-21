7469f20ad7861b2eda8ee23613aeacd463f8e339
parth <parth.p.ajudiya@gmail.com>
2020-04-21 12:26:56 +0530

    update

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,7 +1,7 @@
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
-require_once APPPATH . 'core/controller.php';
+require_once APPPATH . 'core/Controller.php';
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
